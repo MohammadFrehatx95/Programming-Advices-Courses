effe8cf91347f9015911fbed066067c23fbfb977
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-09-27 16:49:46 +0300

    Add files via upload

---
+++ Course level 3 (C++)/Lesson 12# part 1 hw/ConsoleApplication1/ConsoleApplication1/homework#12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "*********************************\n";
+    cout << "Name: Mohammed Abu-Hadhoud." << endl;
+    cout << "Age: 44 Years." << endl;
+    cout << "City: Amman." << endl;
+    cout << "Country: Jordan." << endl;
+    cout << "*********************************\n";
+
+}
+
+
