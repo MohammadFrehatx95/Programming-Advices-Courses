4b95578ce0c966901d6e86edc8ed085e41f39b62
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-09-27 17:24:21 +0300

    Add files via upload

---
+++ Lesson 42# IF, IF ELSE 2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int mark;
+	cout << "Enter Your Mark" << endl;
+	cin >> mark;
+
+	if (mark >= 50)
+	{
+		cout << "Pass" << endl;
+	}
+	else
+	{
+		cout << "Fail" << endl;
+	}
+}
