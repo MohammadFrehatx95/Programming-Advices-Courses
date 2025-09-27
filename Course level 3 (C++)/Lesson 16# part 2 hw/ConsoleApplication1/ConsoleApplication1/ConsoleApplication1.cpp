75a59e28ce1580ee249aa666d78f7a3c488f0260
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-09-27 16:56:11 +0300

    Add files via upload

---
+++ Course level 3 (C++)/Lesson 16# part 2 hw/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Dear Sir\\Madam,\n\nHow are you?\n" << endl;
+    cout << "My name is \"Mohammad\" , nice to meet you." << endl;
+}
+
